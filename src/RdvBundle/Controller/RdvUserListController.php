5d0fc61d3448e3118cf0fa2be9cd521ffa5ea02e
Dylio <>
2018-04-17 20:08:20 +0200

    Implémentation Rdv User List

---
+++ src/RdvBundle/Controller/RdvUserListController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RdvBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RdvUserListController extends Controller {
+
+    public function indexAction(Request $request) {
+        $isAjax = $request->isXmlHttpRequest();
+
+        // Get your Datatable ...
+        //$datatable = $this->get('app.datatable.post');
+        //$datatable->buildDatatable();
+        // or use the DatatableFactory
+        /** @var DatatableInterface $datatable */
+        $datatable = $this->get('app.datatable.rdvuserlist');
+        $datatable->buildDatatable();
+
+        if ($isAjax) {
+            $id = $this->get('security.token_storage')->getToken()->getUser()->getId();
+            $responseService = $this->get('sg_datatables.response');
+            $responseService->setDatatable($datatable);
+            $datatableQueryBuilder = $responseService->getDatatableQueryBuilder();
+
+            $qb = $datatableQueryBuilder->getQb();
+            $qb->andWhere("rdv.userId = '" . $id . "'");
+
+            return $responseService->getResponse();
+        }
+
+        return $this->render('RdvBundle:RdvUserList:index.html.twig', array(
+                    'datatable' => $datatable,
+        ));
+    }
+
+}
