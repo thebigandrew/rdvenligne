1fae83ad558e2c879379c790f770f45c8bbaa227
Mathieu Buis <mathieu.buis@limayrac.fr>
2018-04-22 20:01:36 +0200

    Modification du profile utilisateur

--- src/RdvBundle/Controller/DefaultController.php
+++ src/RdvBundle/Controller/DefaultController.php
@@ -3,11 +3,31 @@
 namespace RdvBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use RdvBundle\Form\UserProfileType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('RdvBundle:Default:index.html.twig');
+    }
+    
+    public function userProfileAction(Request $request){
+        $entityManager = $this->getDoctrine()->getManager();
+        $oUser = $this->getUser();
+        $form = $this->createForm(UserProfileType::class, $oUser);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $oUser->setTelephone($form['telephone']->getData());
+            $oUser->setEmail($form['email']->getData());
+            $oUser->setDateNaissance($form['dateNaissance']->getData());
+            $oUser->setLastname($form['lastName']->getData());
+            $oUser->setFirstname($form['firstName']->getData());
+            $oUser->setUsername($form['userName']->getData());
+            $entityManager->flush();
+            return $this->render('RdvBundle:Default:userprofile.html.twig', array('form' => $form->createView()));
+        }
+        return $this->render('RdvBundle:Default:userprofile.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/RdvBundle/Form/UserProfileType.php
@@ -0,0 +1,34 @@
+<?php
+// src/AppBundle/Form/RegistrationType.php
+
+namespace RdvBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class UserProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('userName', TextType::class, ['label'=>'Login'])
+                ->add('firstName', TextType::class, ['label'=>'Prénom'])
+                ->add('lastName', TextType::class, ['label'=>'Nom'])
+                ->add('email', TextType::class, ['label'=>'Email'])
+                ->add('telephone', TextType::class, ['label'=>'Téléphone'])
+                ->add('dateNaissance', DateType::class, ['label'=>'Date de naissance']);
+    }
+ 
+   public function getBlockPrefix()
+   {
+       return 'app_user_profile';
+   }
+ 
+   public function getName()
+   {
+       return $this->getBlockPrefix();
+   }
+}
