43838fea9bcef042ce781eccb106bafe73ff3a45
Vincent ABET <vincentabet@gmail.com>
2018-04-26 20:17:06 +0200

    edit config

--- src/RdvBundle/Controller/DefaultController.php
+++ src/RdvBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace RdvBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('RdvBundle:Default:index.html.twig');
+    }
+
+    public function testLayout(){
+        return $this->render('@Rdv/Default/dashboard.twig');
     }
 }
