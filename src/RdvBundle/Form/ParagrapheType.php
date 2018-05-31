3b96cc1c9c460509df6c5bd69d762c0e0fedaaee
Oliver-Gibson <__@MSI>
2018-05-31 20:26:15 +0200

    fix pagePersoV2 merge

---
+++ src/RdvBundle/Form/ParagrapheType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RdvBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ParagrapheType extends AbstractType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('titre', TextType::class, ['label' => 'titre'])
+                ->add('dateCreation', DateTimeType::class, ['label' => 'dateCreation'])
+                ->add('dateModification', DateTimeType::class, ['label' => 'dateModification'])
+                ->add('text', TextareaType::class, ['label' => 'texte']);
+    }
+
+    public function getBlockPrefix() {
+        return 'app_paragrpahe';
+    }
+
+    public function getName() {
+        return $this->getBlockPrefix();
+    }
+
+}
--- src/RdvBundle/Repository/ParagrapheRepository.php
+++ src/RdvBundle/Repository/ParagrapheRepository.php
@@ -7,11 +7,4 @@ namespace RdvBundle\Repository;
  */
 class ParagrapheRepository extends \Doctrine\ORM\EntityRepository {
     
-    public function findParagraphe($pro_id) {
-        return $this->createQueryBuilder('p')
-                        ->where('p.pro_id = :pro_id')
-                        ->setParameter('pro_id', $pro_id)
-                        ->orderBy('c.ville', 'ASC')
-                        ->getQuery();
-    }
 }
