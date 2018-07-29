76de18c7af9d20ded2fe1984e412953ace9d3b5e
Oliver-Gibson <>
2018-07-29 16:37:19 +0200

    fix datatable

--- src/RdvBundle/Controller/UserProListController.php
+++ src/RdvBundle/Controller/UserProListController.php
@@ -8,33 +8,39 @@ use Symfony\Component\HttpFoundation\Request;
 class UserProListController extends Controller {
 
     public function indexAction(Request $request) {
-        $isAjax = $request->isXmlHttpRequest();
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $isAjax = $request->isXmlHttpRequest();
 
-        // Get your Datatable ...
-        //$datatable = $this->get('app.datatable.post');
-        //$datatable->buildDatatable();
-        // or use the DatatableFactory
-        /** @var DatatableInterface $datatable */
-        $datatable = $this->get('app.datatable.user_pro_datatable');
-        $datatable->buildDatatable();
+            // Get your Datatable ...
+            //$datatable = $this->get('app.datatable.post');
+            //$datatable->buildDatatable();
+            // or use the DatatableFactory
+            /** @var DatatableInterface $datatable */
+            $datatable = $this->get('app.datatable.user_pro_datatable');
+            $datatable->buildDatatable();
 
-        if ($isAjax) {
-            $id = $this->get('security.token_storage')->getToken()->getUser()->getId();
-            $responseService = $this->get('sg_datatables.response');
-            $responseService->setDatatable($datatable);
-            $datatableQueryBuilder = $responseService->getDatatableQueryBuilder();
+            if ($isAjax) {
+                $id = $this->get('security.token_storage')->getToken()->getUser()->getId();
+                $responseService = $this->get('sg_datatables.response');
+                $responseService->setDatatable($datatable);
+                $datatableQueryBuilder = $responseService->getDatatableQueryBuilder();
 
-            $qb = $datatableQueryBuilder->getQb();
-            $qb->select('userId.lastname, userId.firstname, userId.telephone, MAX(rdv.creneauxDebut) as creneauxDebut, MAX(rdv.creneauxFin) as creneauxFin')
-                    ->andWhere("rdv.proId = '" . $id . "'")
-                    ->groupBy('rdv.userId');
+                $qb = $datatableQueryBuilder->getQb();
+                $qb->select('userId.lastname, userId.firstname, userId.telephone, MAX(rdv.creneauxDebut) as creneauxDebut, MAX(rdv.creneauxFin) as creneauxFin')
+                        ->andWhere("rdv.proId = :proId")
+                        ->andWhere("rdv.userId IS NOT NULL")
+                        ->groupBy('rdv.userId')
+                        ->setParameter('proId', $id);
 
-            return $responseService->getResponse();
+                return $responseService->getResponse();
+            }
+
+            return $this->render('RdvBundle:RdvUserList:index.html.twig', array(
+                'datatable' => $datatable,
+            ));
+        } else {
+            return $this->redirectToRoute('fos_user_security_login');
         }
-
-        return $this->render('RdvBundle:RdvUserList:index.html.twig', array(
-                    'datatable' => $datatable,
-        ));
     }
 
 }
